490733373a98f2cbb8b299435334b12b4b928142
王俊仁 <jr-wong@qq.com>
2017-08-01 15:26:21 +0800

    add umbrella.h

---
+++ Classes/MethodCopyer.h
@@ -0,0 +1,15 @@
+//
+//  MethodCopyer.h
+//  MethodCopyer
+//
+//  Created by 王俊仁 on 2017/8/1.
+//  Copyright © 2017年 J. All rights reserved.
+//
+
+#ifndef MethodCopyer_h
+#define MethodCopyer_h
+
+#import <MethodCopyer/MCMethodCopyer.h>
+#import <MethodCopyer/NSObject+MCTestClassMethod.h>
+
+#endif /* MethodCopyer_h */
